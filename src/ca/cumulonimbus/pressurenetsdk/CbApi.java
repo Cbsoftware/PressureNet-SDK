d13e186f3da1011389e6967266afea9f402fa2b4
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-05-01 21:01:34 -0400

    Increasing default limit to 10000

--- src/ca/cumulonimbus/pressurenetsdk/CbApi.java
+++ src/ca/cumulonimbus/pressurenetsdk/CbApi.java
@@ -112,7 +112,7 @@ public class CbApi {
 				nvps.add(new BasicNameValuePair("end_time", apiCall.getEndTime() + ""));
 				nvps.add(new BasicNameValuePair("api_key", apiCall.getApiKey()));
 				nvps.add(new BasicNameValuePair("format", "json"));
-				nvps.add(new BasicNameValuePair("limit", "5000")); // TODO: User preference
+				nvps.add(new BasicNameValuePair("limit", "10000")); // TODO: User preference
 
 				String paramString = URLEncodedUtils.format(nvps, "utf-8");
 
