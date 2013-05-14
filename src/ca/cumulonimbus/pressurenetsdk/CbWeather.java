d2edd4b2f537a4198296b815080d70309a29b1e8
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-05-14 09:40:11 -0400

    Support map positioning in time with slider

--- src/ca/cumulonimbus/pressurenetsdk/CbWeather.java
+++ src/ca/cumulonimbus/pressurenetsdk/CbWeather.java
@@ -1,6 +1,18 @@
 package ca.cumulonimbus.pressurenetsdk;
 
+/**
+ * Parent class for all Weather
+ * @author jacob
+ *
+ */
 public class CbWeather {
 
-	
+	private int animateGroupNumber = 0;
+
+	public int getAnimateGroupNumber() {
+		return animateGroupNumber;
+	}
+	public void setAnimateGroupNumber(int animateGroupNumber) {
+		this.animateGroupNumber = animateGroupNumber;
+	}
 }
