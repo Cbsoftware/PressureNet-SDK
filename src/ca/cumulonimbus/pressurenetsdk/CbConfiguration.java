19ef9e31861c7fad4ff0738edffc9d69f53a9897
Jacob Sheehy <jacob.sheehy@gmail.com>
2015-08-12 12:27:28 -0700

    updating version

--- src/ca/cumulonimbus/pressurenetsdk/CbConfiguration.java
+++ src/ca/cumulonimbus/pressurenetsdk/CbConfiguration.java
@@ -13,14 +13,14 @@ public class CbConfiguration {
 	public static final String SERVER_URL_CONDITIONS_SECONDARY		= "";
 	public static final String SERVER_URL_DEVICE_REGISTRATION 		= "";
 	public static final String SERVER_URL_EMAIL_REGISTRATION 		= "";
-	
-	
-	
+
+
+
 	public static final String CB_WEBSITE 			= "http://cumulonimbus.ca/";
 	public static final String API_SIGNUP_URL 		= "https://pressurenet.io/developers/";
 	public static final String API_KEY 				= "YOUR_API_KEY_HERE";
 	public static final String API_PROJECT_NUMBER 	= "";
 	public static final boolean DEBUG_MODE 			= true;
-	public static final String SDK_VERSION 			= "1.6.0";
+	public static final String SDK_VERSION 			= "1.6.2";
 	
 }
